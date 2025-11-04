9d069e56d18ef7804fc22c14aee65acc39d7b540
sudarshanroyal57-psr <sudarshanroyal57@gmail.com>
2025-11-04 19:03:55 +0530

    Create C Program to display a number if it is negative.c

---
+++ C Program to display a number if it is negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n;
+printf("Enter an integer: ");
+scanf("%d",&n);
+if(n<0)
+{
+printf("The entered integer is negative:");
+}
+return 0;
+}
+C Program to dispnumlay a ber if it is negative.c
